c0b1a7543d5e3de3e956fb1166dac4683a80c6ab
Spencer Hakim <spencerhakim@gmail.com>
2013-02-04 18:17:22 -0500

    Renamed TestApp to ImageMapButtonDemoApp

--- ImageMapButtonDemoApp/Properties/AssemblyInfo.cs
+++ ImageMapButtonDemoApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestApp")]
+[assembly: AssemblyTitle("ImageMapButtonDemoApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestApp")]
+[assembly: AssemblyProduct("ImageMapButtonDemoApp")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
