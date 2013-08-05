2a4c770acf98b16ad76cbddc438aad095d5bbfee
Spencer Hakim <spencerhakim@gmail.com>
2013-08-05 19:11:28 -0400

    Disable null dereference warning (since that's exactly what we're trying to do)

--- SpencerHakimNET.Native/NativeExceptions.cpp
+++ SpencerHakimNET.Native/NativeExceptions.cpp
@@ -6,9 +6,11 @@ using namespace SpencerHakim;
 
 void NativeExceptions::ThrowSEHException()
 {
+#pragma warning(disable : 6011)
     //triggers SEH - http://msdn.microsoft.com/en-us/library/swezty51.aspx
     volatile int* p = NULL;
     *p = 1;
+#pragma warning(default : 6011)
 }
 
 void NativeExceptions::ThrowSTDException()
